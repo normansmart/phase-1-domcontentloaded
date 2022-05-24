9c1a294ec3c3c5a53fcb8af586208eefcf9c2169
Norman Smart <normansmart14@gmail.com>
2022-05-24 10:47:39 -0400

    commit tree d9c1be20d5a1eb0b30ed8328ba37596ed2d001e9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    const p = document.getElementById("text");
+
+
+
+    p.textContent = "This is really cool!";
+
+    
+  });
+  
+ 
+
